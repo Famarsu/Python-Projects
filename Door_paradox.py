3ebb12316be5ed0d21e3c959ce20ebe7741349b6
Famarsu <llartuss7@gmail.com>
2023-02-06 15:51:00 +0100

    code cleaned

--- Door_paradox.py
+++ Door_paradox.py
@@ -1,9 +1,22 @@
 import random
 random.seed()
 
+#DEFINITIONS
+def stay_strategy(picked_door):
+    new_door = picked_door
+    return new_door
+
+
+def change_strategy(number_of_doors,picked_door,deleted_door):
+    new_door = (random.randint(1,number_of_doors))
+    while new_door == picked_door or new_door == deleted_door:
+        new_door = (random.randint(1,number_of_doors))
+    return new_door
+
 #INPUTS
-number_of_doors=int(input("number of doors "))
-iterations=int(input("iterations "))
+number_of_doors=int(input("number of doors: "))
+iterations=int(input("iterations: "))
+strategy=(input("strategy (stay/change): "))
 correct = 0
 
 #PLAY
@@ -14,25 +27,15 @@ for i in range(1,iterations+1):
     deleted_door = (random.randint(1,number_of_doors))
     while deleted_door == correct_door or deleted_door == picked_door:
         deleted_door = (random.randint(1,number_of_doors))
-
-    #STAY STRATEGY
-    """comment the CHANGE STRATEGY"""
-    new_door = picked_door
-
-    #CHANGE STRATEGY
-    new_door = (random.randint(1,number_of_doors))
-    while new_door == picked_door or new_door == deleted_door:
-        new_door = (random.randint(1,number_of_doors))
+    #STRATEGY
+    if strategy == "stay":
+        new_door = stay_strategy(picked_door)
+    elif strategy == "change":
+        new_door = change_strategy(number_of_doors,picked_door,deleted_door)
         
-    #OUTPUT
-    """ print("Deleted door was n: " + str(deleted_door))
-    print("Picked_door was n: " + str(picked_door))
-    print("Correct door was n: " + str(correct_door)) """
+    #RESULT
     if correct_door == new_door:
         correct += 1
-"""         print("YAAAY")
-    else:
-        print("NAAAY") """
 
 #STATISTICS
 print("The strategy succeds in a " + str(100*correct/iterations) + "% of the cases")
